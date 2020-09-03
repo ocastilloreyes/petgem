fa2caa0eb223046b05c499bbab3583ae4a797af1
ocastilloreyes <ocastilloreyes@gmail.com>
2020-09-03 17:30:27 +0200

    Add codacy

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -14,7 +14,7 @@ def test_mesh_functions():
 
     # Setup mesh file name
     mesh_filename = 'tests/data/test_mesh.msh'
-    basis_order = 2
+
     # Read nodes
     nodes, _ = readGmshNodes(mesh_filename)
     nNodes = nodes.shape[0]
@@ -32,20 +32,20 @@ def test_mesh_functions():
 
     try:
         nElems == 9453
-    except mesh_error:
+    except:
         print("Number of elements is not consistent.")
 
     try:
         nNodes == 2163
-    except mesh_error:
+    except:
         print("Number of nodes is not consistent.")
 
     try:
         nFaces == 20039
-    except mesh_error:
+    except:
         print("Number of faces is not consistent.")
 
     try:
         nEdges == 12748
-    except mesh_error:
+    except:
         print("Number of edges is not consistent.")
